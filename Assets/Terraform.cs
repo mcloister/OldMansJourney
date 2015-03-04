58593979a77fb93da344216e78f71a4fe57362ea
Felix Bohatsch <felix@brokenrul.es>
2015-03-04 15:42:50 +0100

    Multiple splines!
    Click/Touch chooses closest spline and then closest control point to
    that to transform

--- Assets/Terraform.cs
+++ Assets/Terraform.cs
@@ -6,32 +6,36 @@ public class Terraform : MonoBehaviour {
 	public GameObject currentNode;
 	public float influence = 1;
 
-	private Spline spline;
+	private GameObject[] allSplines;
+	public Spline spline;
 	private Vector3 lastMousePos;
 
 	// Use this for initialization
 	void Start () {
-		spline = GetComponent<Spline> ();
-
+		allSplines = GameObject.FindGameObjectsWithTag("Spline");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) 
 		{
+			Vector3 mouse = Input.mousePosition;
+			Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3 (mouse.x, mouse.y, -Camera.main.gameObject.transform.position.z));
+
+			Debug.Log ("finding closes spline to mouse pos: " + mouse);
+
+			findClosestSpline(mouseWorld);
+
 			outputControlPoints();
 
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			float param = spline.GetClosestPointParamToRay(ray, 5);
+			currentNode = getClosestObject(spline.GetClosestPointParam(mouseWorld, 3));
 
-			currentNode = getClosestObject(param);
-
-			lastMousePos = adjustMousePos(Input.mousePosition);
+			lastMousePos = normalizeMousePos(mouse);
 		}
 
 		if (Input.GetMouseButton (0)) 
 		{
-			Vector3 mousePos = adjustMousePos(Input.mousePosition);
+			Vector3 mousePos = normalizeMousePos(Input.mousePosition);
 
 			Vector3 mousePosDiff = mousePos - lastMousePos;
 			
@@ -50,6 +54,25 @@ public class Terraform : MonoBehaviour {
 		}
 	}
 
+	private void findClosestSpline(Vector3 mouseWorld)
+	{
+		float minDist = Mathf.Infinity;
+		foreach(GameObject o in allSplines)
+		{
+			Spline s = o.GetComponent<Spline>();
+
+			Vector3 onSpline = s.GetPositionOnSpline(s.GetClosestPointParam(mouseWorld, 3));
+			float dist = (onSpline - mouseWorld).magnitude;
+
+			Debug.Log ("pos on spline: " + onSpline + " dist: " + dist);
+
+			if(dist < minDist)
+			{
+				minDist = dist;
+				spline = s;
+			}
+		}
+	}
 
 	private GameObject getClosestObject(float param)
 	{
@@ -57,8 +80,12 @@ public class Terraform : MonoBehaviour {
 
 		float minParamDifference = Mathf.Infinity;
 		GameObject closestObject = null;
-		foreach (SplineNode n in allNodes) 
+
+		//we don't allow the first and the last control point to be moved
+		for (int i = 1; i < allNodes.Length-1; i++) 
 		{
+			SplineNode n = allNodes[i];
+
 			float paramDifference = Mathf.Abs(param - n.Parameters[spline].PosInSpline);
 			if(paramDifference < minParamDifference)
 			{
@@ -75,14 +102,9 @@ public class Terraform : MonoBehaviour {
 		return closestObject;
 	}
 
-	private Vector3 adjustMousePos(Vector3 mousePos)
+	private Vector3 normalizeMousePos(Vector3 mousePos)
 	{
-
 		Vector3 adjustedMousePos = mousePos;
-
-		//switch z and y because our scene is set up this way
-//		adjustedMousePos.z = adjustedMousePos.y;
-//		adjustedMousePos.y = 0;
 
 		//normalize it to current screen resolution
 		adjustedMousePos.x /= Screen.width;

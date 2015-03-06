e6f4027e11717ee9fe6c85d24e59d941cddac4b2
Felix Bohatsch <felix@brokenrul.es>
2015-03-06 16:46:46 +0100

    Added simple goal

---
+++ Assets/StayOnPath.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StayOnPath : MonoBehaviour {
+
+	public Spline spline;
+
+	float param;
+	// Use this for initialization
+	void Start () 
+	{
+		if (spline == null)
+			return;
+
+		param = spline.GetClosestPointParam (transform.position, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = spline.GetPositionOnSpline (param);
+	}
+}

18ba5633dc4fea3ee33a13819a3133ed1d95fd44
Felix Bohatsch <felix@brokenrul.es>
2015-03-10 16:07:55 +0100

    added (invisible) waterfall

--- Assets/Scripts/MoveOnSpline.cs
+++ Assets/Scripts/MoveOnSpline.cs
@@ -14,15 +14,22 @@ public class MoveOnSpline : MonoBehaviour {
 	Spline[] allSplines;
 	private Spline oldSpline;
 	Dictionary<Spline, float> paramOnSplines;
+
 	Vector3 targetMousePos;
 	Vector3 mousePosOnDown = Vector3.zero;
 	Vector3 mouseDelta = Vector3.zero;
 	Vector3 posOnDown;
+
+	float initialSpeed = 0.0f;
+
 	int direction;
 
 	// Use this for initialization
 	void Start () 
 	{
+
+		initialSpeed = speed;
+
 		GameObject[] splineObjects =  GameObject.FindGameObjectsWithTag("Spline");
 
 		allSplines = new Spline[splineObjects.Length];
@@ -94,6 +101,7 @@ public class MoveOnSpline : MonoBehaviour {
 
 //			float nextP = paramOnSplines[spline] + speed * direction * Time.deltaTime;
 			Vector3 pos = spline.GetPositionOnSpline(paramOnSplines[spline]);
+			Vector3 posOnScreen = Camera.main.WorldToScreenPoint(pos);
 			spline.transform.Find("CharacterPos").position = pos;
 
 			//now check if we need to switch spline
@@ -104,23 +112,32 @@ public class MoveOnSpline : MonoBehaviour {
 				if(s.GetInstanceID() == spline.GetInstanceID())
 					continue;
 
-				float otherP = s.GetClosestPointParamToRay(Camera.main.ScreenPointToRay(Camera.main.WorldToScreenPoint(pos)), 3);
+				float otherP = s.GetClosestPointParamToRay(Camera.main.ScreenPointToRay(posOnScreen), 3);
 				Vector3 otherPos = s.GetPositionOnSpline(otherP);
 
 				s.transform.Find("CharacterPos").position = otherPos;
 
+				//TODO; check this
+//				float dis = Vector3.Distance(pos, Camera.main.WorldToScreenPoint(otherPos));
 				float dis = Vector3.Distance(pos, otherPos) - Mathf.Abs (spline.transform.position.z - s.transform.position.z);
 
 				Debug.Log(spline.name + " & " + s.name + " dis: " + dis);
 
 				if(dis < switchThreshold)
 				{
+					Waterfall waterfall = s.GetComponent<Waterfall>();
+					if(waterfall != null)
+					{
+						if(waterfall.direction != direction)
+							continue;
+					}
+
 					Vector3 tangent = spline.GetTangentToSpline(paramOnSplines[spline]) * direction;
 					Vector3 otherTangent = s.GetTangentToSpline(otherP) * direction;
 					Debug.Log (spline.name + " & " + s.name + " are crossing! t: " + tangent + " oT: " + otherTangent);
 
 
-					if(otherTangent.y >	 tangent.y)// || spline.transform.position.z == s.transform.position.z && (oldSpline == null || oldSpline.GetInstanceID() != s.GetInstanceID()) )
+					if(otherTangent.y >	tangent.y)// || spline.transform.position.z == s.transform.position.z && (oldSpline == null || oldSpline.GetInstanceID() != s.GetInstanceID()) )
 					{
 //						oldSpline = spline;
 //						StartCoroutine(forgetOldSpline());
@@ -129,7 +146,19 @@ public class MoveOnSpline : MonoBehaviour {
 						paramOnSplines[s] = otherP;
 						updateTransform ();
 
-						setTarget (targetMousePos);
+						if(waterfall!=null)
+						{
+							speed = waterfall.fallSpeed;
+
+							Vector3 screenBottom = Camera.main.ScreenToWorldPoint(new Vector3(0,0, -Camera.main.transform.position.z + transform.position.z));
+
+							setTarget(new Vector3(transform.position.x, screenBottom.y, transform.position.z));
+						}
+						else
+						{
+							speed = initialSpeed;
+							setTarget (targetMousePos);
+						}
 					}
 				}
 
---
+++ Assets/Waterfall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waterfall : MonoBehaviour {
+	public int direction = 1;
+
+	public float fallSpeed = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		if (fallSpeed == 0.0f)
+			fallSpeed = GameObject.FindGameObjectWithTag ("Character").GetComponent<MoveOnSpline> ().speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

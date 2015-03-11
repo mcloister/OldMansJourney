1bf2fdd9f8150bd357b104646b4bfca81841b461
Felix Bohatsch <felix@brokenrul.es>
2015-03-11 17:09:12 +0100

    added primitive crank
    uses unity event system. con: it only registers drag while a drag is
    happening on top of the object

---
+++ Assets/AddForceFromMouse.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class AddForceFromMouse : MonoBehaviour, IBeginDragHandler, IDragHandler {
+
+	public Vector3 startPosition;
+	public Vector3 currentForce;
+	public float forceFactor = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		startPosition = eventData.worldPosition;
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		currentForce = eventData.worldPosition - startPosition * forceFactor;
+
+		rigidbody.AddForceAtPosition (currentForce, startPosition);
+
+	}
+}

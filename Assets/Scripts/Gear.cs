295ac01640745375d0bda0f9ac7aca6df7e019ae
Felix Bohatsch <felix@brokenrul.es>
2015-03-12 14:35:31 +0100

    moved scripts into scripts folder

--- Assets/Scripts/Gear.cs
+++ Assets/Scripts/Gear.cs
@@ -6,11 +6,14 @@ public class Gear : MonoBehaviour {
 	public float ratio = 1;
 	public float transmission;
 	public Vector2 limits = new Vector2(0,1);
+	public float activationThreshold = 0.01f;
+
+	public bool moving;
 
 	float lastRotationZ;
 	// Use this for initialization
 	void Start () {
-		transmission = 0;
+		transmission = limits.x;
 	}
 	
 	// Update is called once per frame
@@ -18,13 +21,15 @@ public class Gear : MonoBehaviour {
 	{
 		float rotationZ = transform.localRotation.eulerAngles.z;
 
-//		if (Mathf.DeltaAngle (lastRotationZ, rotationZ) > Mathf.Epsilon) 
-//		{
+		if (Mathf.Abs (Mathf.DeltaAngle (lastRotationZ, rotationZ)) > activationThreshold) {
+			moving = true;
+
 			transmission += Mathf.DeltaAngle (lastRotationZ, rotationZ) * ratio * Time.deltaTime;
 
 			transmission = Mathf.Clamp (transmission, limits.x, limits.y);
-//		}
-
+		} else
+			moving = false;
+		
 		lastRotationZ = rotationZ;
 	}
 }

1ded508090b6dd77f7d64f352ec920e534e6a48d
Felix Bohatsch <felix@brokenrul.es>
2015-03-12 14:35:47 +0100

    scaling a bridge through gear

---
+++ Assets/Scripts/ScaleThroughGear.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleThroughGear : MonoBehaviour {
+
+	public Gear gear;
+	public Vector3 affectedAxes = Vector3.one;
+	public int scaleDirection = -1; //-1 = left, 0 = symmetrical, 1 = right
+
+	Vector3 initialScale;
+	Vector3 initialPosition;
+
+	// Use this for initialization
+	void Start () {
+		initialScale = transform.localScale;
+		initialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!gear.moving)
+			return;
+
+		Vector3 currentScale = affectedAxes * gear.transmission;
+
+		transform.localScale = currentScale + initialScale;
+
+		transform.position = (scaleDirection * currentScale)/2 + initialPosition;
+	}
+}

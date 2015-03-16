366d49fd335c0e2d5fdb69230a67a2c2b1d9cbd9
Felix Bohatsch <felix@brokenrul.es>
2015-03-16 16:13:13 +0100

    rolling ball in debug level

--- Assets/Scripts/StayOnPath.cs
+++ Assets/Scripts/StayOnPath.cs
@@ -7,9 +7,17 @@ public class StayOnPath : MonoBehaviour {
 	public Vector3 offset;
 
 	float param;
+	
+	
+	SplineSelector selector;
+
 	// Use this for initialization
 	void Start () 
 	{
+		GameObject controller = GameObject.FindGameObjectWithTag ("GameController");
+		if(controller != null)
+			selector = controller.GetComponent<SplineSelector> ();
+
 		if (spline == null)
 			return;
 
@@ -18,7 +26,11 @@ public class StayOnPath : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () 
-	{
-		transform.position = spline.GetPositionOnSpline (param) + offset;
+	{		
+		if(Input.GetMouseButtonDown(0))
+			param = spline.GetClosestPointParam (transform.position, 3);
+
+		if(selector != null && selector.spline.GetInstanceID() == spline.GetInstanceID())
+			transform.position = spline.GetPositionOnSpline (param) + offset;
 	}
 }

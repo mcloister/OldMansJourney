7c65854da0094f4c7b50b846946f7e6e06c89a0a
Felix Bohatsch <felix@brokenrul.es>
2015-04-01 12:45:16 +0200

    For now GameController just setups UnityDebugg. Might be useful for other things as well

---
+++ Assets/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityDebugger;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debugger.LogLevel = UnityDebugger.LogLevel.Info;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
